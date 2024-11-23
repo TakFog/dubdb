10ecc70d44c36e3703f16fc402ac6c84e516b90e
TakFog <takfog@gmail.com>
2024-11-23 14:24:47 +0100

    dubbers: implementation

--- create/src/main/kotlin/takutility/dubdb/ops/dubber/Dubbers.kt
+++ create/src/main/kotlin/takutility/dubdb/ops/dubber/Dubbers.kt
@@ -1,10 +1,22 @@
 package takutility.dubdb.ops.dubber
 
 import takutility.dubdb.DubDbContext
+import takutility.dubdb.m
+import takutility.dubdb.tasks.wikiapi.DubbersFromCategory
 
 class Dubbers(val context: DubDbContext) {
 
     fun run(num: Int) {
-        TODO()
+        var dubbers = context.dubEntityDb.findMostCommonDubbers(num)
+        if (dubbers.size < num) {
+            context.m<DubbersFromCategory>().run(num)
+            dubbers = context.dubberDb.findMostRecent(num)
+        }
+
+        dubbers.asSequence()
+            .mapNotNull { it.wikiId }
+            .map { context.wikiPageLoader.page(it) }
+            .filter { it.exists() }
+            .forEach { context.m<ExtractDubber>().run(it) }
     }
 }
