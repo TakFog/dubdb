2c69ed8f21846e3037e2a7e71a0ac8958b00cdc6
TakFog <takfog@gmail.com>
2024-12-14 14:41:04 +0100

    dubbers: popularity implementation

--- create/src/main/kotlin/takutility/dubdb/ops/dubber/Dubbers.kt
+++ create/src/main/kotlin/takutility/dubdb/ops/dubber/Dubbers.kt
@@ -1,6 +1,7 @@
 package takutility.dubdb.ops.dubber
 
 import takutility.dubdb.DubDbContext
+import takutility.dubdb.entities.DubberRef
 import takutility.dubdb.m
 import takutility.dubdb.tasks.wikiapi.DubbersFromCategory
 
@@ -8,9 +9,11 @@ class Dubbers(val context: DubDbContext) {
 
     fun run(num: Int) {
         var dubbers = context.dubEntityDb.findMostCommonDubbers(num)
-        if (dubbers.size < num) {
+        if (notEnoughDubbers(num, dubbers)) {
             context.m<DubbersFromCategory>().run(num)
             dubbers = context.dubberDb.findMostRecent(num)
+            val pop = context.dubEntityDb.countDubbers(dubbers)
+            dubbers = dubbers.sortedByDescending { pop.getOrDefault(it, 0) }
         }
 
         dubbers.asSequence()
@@ -18,5 +21,20 @@ class Dubbers(val context: DubDbContext) {
             .map { context.wikiPageLoader.page(it) }
             .filter { it.exists() }
             .forEach { context.m<ExtractDubber>().run(it) }
+    }
+
+    private fun notEnoughDubbers(num: Int, dubbers: List<DubberRef>): Boolean {
+        if (dubbers.size < num)
+            return true
+
+        val lastPop = context.dubEntityDb.countDubber(dubbers.last())
+        if (lastPop < 10)
+            return true
+
+        val halfPop = context.dubEntityDb.countDubber(dubbers[dubbers.size/2])
+        if (halfPop == lastPop)
+            return true
+
+        return false
     }
 }
