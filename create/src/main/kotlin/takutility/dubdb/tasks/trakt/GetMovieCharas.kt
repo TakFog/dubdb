f410522211c68b8cb40ec9afd8c5afe5c7928f12
TakFog <takfog@gmail.com>
2025-04-06 15:11:03 +0200

    trakt characters from movie: implementation

--- create/src/main/kotlin/takutility/dubdb/tasks/trakt/GetMovieCharas.kt
+++ create/src/main/kotlin/takutility/dubdb/tasks/trakt/GetMovieCharas.kt
@@ -1,13 +1,40 @@
 package takutility.dubdb.tasks.trakt
 
 import takutility.dubdb.DubDbContext
-import takutility.dubdb.entities.MovieRef
+import takutility.dubdb.entities.*
+import takutility.dubdb.entities.Source.IMDB
+import takutility.dubdb.entities.Source.TRAKT
 import takutility.dubdb.tasks.TaskResult
 
 class GetMovieCharas(context: DubDbContext) {
     private val trakt = context.trakt
 
     fun run(movie: MovieRef): TaskResult {
-        TODO()
+        val traktId = movie.traktId ?: return TaskResult.empty
+        val trackSourceId = movie.ids[TRAKT]!!
+
+        val entities = trakt.movieCredits(traktId)?.asSequence()?.flatMap { credit ->
+            credit.person
+                ?.run {
+                    ActorRefImpl(
+                        name, SourceIds.of(
+                            TRAKT to ids.trakt?.toString(), IMDB to ids.imdb
+                        )
+                    )
+                }
+                ?.let { actor ->
+                    credit.characters?.asSequence()?.map { name ->
+                        DubbedEntity(
+                            name = name,
+                            movie = movie,
+                            actor = actor,
+                            sources = mutableListOf(RawData(trackSourceId, DataSource.TRAKT, name))
+                        )
+                    }
+                }
+                ?: sequenceOf()
+        }?.toList() ?: return TaskResult.empty
+
+        return TaskResult(dubbedEntities = entities)
     }
 }
