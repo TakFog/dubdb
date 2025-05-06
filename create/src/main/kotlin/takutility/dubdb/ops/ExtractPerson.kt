35972902af14f7391b075ece3b8276326600250e
TakFog <takfog@gmail.com>
2025-05-06 15:58:38 +0200

    extract person refactoring

---
+++ create/src/main/kotlin/takutility/dubdb/ops/ExtractPerson.kt
@@ -0,0 +1,67 @@
+package takutility.dubdb.ops
+
+import takutility.dubdb.DubDbContext
+import takutility.dubdb.db.EntityRepository
+import takutility.dubdb.entities.Entity
+import takutility.dubdb.entities.EntityRef
+import takutility.dubdb.entities.Source
+import takutility.dubdb.entities.SourceIds
+import takutility.dubdb.tasks.wiki.FindPhoto
+import takutility.dubdb.tasks.wiki.ReadIds
+import takutility.dubdb.wiki.WikiPage
+import java.time.Instant
+
+abstract class ExtractPerson<E>(val context: DubDbContext) where E: Entity, E: EntityRef {
+    abstract val db: EntityRepository<E>
+
+    fun run(page: WikiPage): E {
+        /*
+        Leggi id wiki
+        Leggi titolo wiki
+        Leggi pagina wiki
+        Leggi foto wiki
+        Altri id
+
+        Salva persona
+         */
+
+        val ids = readIds(page)
+
+        val person = getPerson(ids)
+        person.ids += context[FindPhoto::class].run(person).sourceIds
+        moreIds(person)
+
+        person.parseTs = Instant.now()
+        db.save(person)
+        updateRefIds(listOf(person))
+
+        withPerson(person, page)
+
+        return person
+    }
+
+    private fun readIds(page: WikiPage): SourceIds {
+        val ids = SourceIds.of(Source.WIKI to page.title)
+        ids += context[ReadIds::class].run(page).sourceIds
+        return ids
+    }
+
+    protected abstract fun updateRefIds(persons: List<E>)
+    protected abstract fun newPerson(ids: SourceIds): E
+
+    protected open fun moreIds(person: E) {}
+    protected open fun withPerson(person: E, page: WikiPage) {}
+
+    private fun getPerson(ids: SourceIds): E {
+        val results = db.findBySources(ids)
+
+        return if (results.size == 1) {
+            val found = results[0]
+            found.ids += ids
+            found
+        } else {
+            newPerson(ids)
+        }
+    }
+
+}
--- create/src/main/kotlin/takutility/dubdb/ops/dubber/ExtractDubber.kt
+++ create/src/main/kotlin/takutility/dubdb/ops/dubber/ExtractDubber.kt
@@ -3,52 +3,21 @@ package takutility.dubdb.ops.dubber
 import takutility.dubdb.DubDbContext
 import takutility.dubdb.entities.DubbedEntity
 import takutility.dubdb.entities.Dubber
-import takutility.dubdb.entities.Source
 import takutility.dubdb.entities.SourceIds
-import takutility.dubdb.tasks.wiki.FindPhoto
+import takutility.dubdb.ops.ExtractPerson
 import takutility.dubdb.tasks.wiki.ReadDubberSection
-import takutility.dubdb.tasks.wiki.ReadIds
 import takutility.dubdb.wiki.WikiPage
-import java.time.Instant
 
-class ExtractDubber(val context: DubDbContext) {
+class ExtractDubber(context: DubDbContext): ExtractPerson<Dubber>(context) {
+    override val db = context.dubberDb
 
-    fun run(page: WikiPage): Dubber {
-        /*
-        Leggi id wiki
-        Leggi titolo wiki
-        Leggi pagina wiki
-        Leggi foto wiki
-        Salva doppiatore
-        Salva personaggi
-         */
+    override fun updateRefIds(persons: List<Dubber>) = context.dubEntityDb.updateRefIds(persons)
 
-        val ids = SourceIds.of(Source.WIKI to page.title)
-        ids += context[ReadIds::class].run(page).sourceIds
+    override fun newPerson(ids: SourceIds) = Dubber("", ids = ids)
 
-        val dubber = getDubber(ids)
-        dubber.ids += context[FindPhoto::class].run(dubber).sourceIds
-
-        dubber.parseTs = Instant.now()
-        context.dubberDb.save(dubber)
-        context.dubEntityDb.updateRefIds(listOf(dubber))
-
-        context[ReadDubberSection::class].run(dubber, page).dubbedEntities
-            ?.let { saveEntities(dubber, it) }
-
-        return dubber
-    }
-
-    private fun getDubber(ids: SourceIds): Dubber {
-        val results = context.dubberDb.findBySources(ids)
-
-        return if (results.size == 1) {
-            val found = results[0]
-            found.ids += ids
-            found
-        } else {
-            Dubber("", ids = ids)
-        }
+    override fun withPerson(person: Dubber, page: WikiPage) {
+        context[ReadDubberSection::class].run(person, page).dubbedEntities
+            ?.let { saveEntities(person, it) }
     }
 
     private fun saveEntities(dubber: Dubber, entities: List<DubbedEntity>) {
